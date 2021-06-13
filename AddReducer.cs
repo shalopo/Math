4a97ad4aabbd3f03bf7398d1c96f98f68934d49e
shalo <shalo@DESKTOP-DFOK1EV>
2021-06-14 00:42:53 +0300

    Fix reduce to const complex

--- AddReducer.cs
+++ AddReducer.cs
@@ -22,7 +22,7 @@ namespace MathUtil
 
                 if (constComplex != null)
                 {
-                    return null;
+                    return constComplex;
                 }
             }
 
